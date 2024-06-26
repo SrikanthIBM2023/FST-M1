11f9ac3f8d6c4f78a8f03c318aa7096d86a45727
SrikanthIBM2023 <srikanth.kundala@ibm.com>
2024-06-26 15:09:17 +0530

    Adding Appium project activities

--- Appium/Project/Activity1.java
+++ Appium/Project/Activity1.java
@@ -1,62 +1,63 @@
-package liveProject;
+package project;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
 
 public class Activity1 {
+
     AndroidDriver driver;
     WebDriverWait wait;
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
-        UiAutomator2Options task = new UiAutomator2Options()
-                .setPlatformName("Android")
+        UiAutomator2Options caps = new UiAutomator2Options()
+                .setPlatformName("android")
                 .setAutomationName("UiAutomator2")
                 .setAppPackage("com.google.android.apps.tasks")
                 .setAppActivity(".ui.TaskListsActivity")
                 .noReset();
 
-        //Set the server URL
-        URL serverURL = new URL("http://localhost:4723/wb/hub");
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        //Initialize the Android driver
-        driver = new AndroidDriver(serverURL, task);
+        driver = new AndroidDriver(serverURL, caps);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
+
     @Test
-    public void TaskTest() throws InterruptedException {
-        //Find the 7 and tap it
-        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/tasks_fab")).click();
+    public void addTask(){
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Complete Activity with Google Tasks");
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Complete Activity with Google Keep");
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Complete the second Activity Google Keep");
+        driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+        List<WebElement> tasks = driver.findElements(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"com.google.android.apps.tasks:id/task_name\"]"));
+        Assert.assertEquals(tasks.get(2).getText(), "Complete Activity with Google Tasks");
+        Assert.assertEquals(tasks.get(1).getText(), "Complete Activity with Google Keep");
+        Assert.assertEquals(tasks.get(0).getText(), "Complete the second Activity Google Keep");
+    }
 
-        //find elements on the page
-        WebElement addTaskInput = driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='com.google.android.apps.tasks:id/add_task_title']"));
-        WebElement addTaskButton = driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id='com.google.android.apps.tasks:id/add_task_done']"));
-
-        //Enter Tasks
-        addTaskInput.sendKeys("Complete Activity with Google Tasks");
-        addTaskButton.click();
-        Thread.sleep(1000);
-        addTaskInput.sendKeys("Complete Activity with Google Keep");
-        addTaskButton.click();
-        Thread.sleep(1000);
-        addTaskInput.sendKeys("Complete the second Activity Google Keep");
-        addTaskButton.click();
-        Thread.sleep(1000);
-
-        //Assertions
-        String Task1 = driver.findElement(AppiumBy.id("android:id/content")).getText();
-        Assert.assertEquals(Task1, "Complete Activity with Google Tasks");
-        String Task2 = driver.findElement(AppiumBy.id("android:id/content")).getText();
-        Assert.assertEquals(Task2, "Complete Activity with Google Keep");
-        String Task3 = driver.findElement(AppiumBy.id("android:id/content")).getText();
-        Assert.assertEquals(Task3, "Complete the second Activity Google Keep");
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 }
--- Appium/Project/Activity4.java
+++ Appium/Project/Activity4.java
@@ -1,8 +1,7 @@
-package Project;
+package project;
 
-
+import activities.ActionsBase;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.Dimension;
@@ -11,26 +10,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-
-import static activities.W3CActionsBase.doSwipe;
+import java.util.List;
 
 public class Activity4 {
-    // Driver Declaration
+
     AndroidDriver driver;
     WebDriverWait wait;
 
-    // Set up method
     @BeforeClass
-    public void setUp() throws MalformedURLException {
-        // Desired Capabilities
+    public void SetUp() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setAutomationName("UiAutomator2");
@@ -38,49 +32,45 @@ public class Activity4 {
         options.setAppActivity("com.google.android.apps.chrome.Main");
         options.noReset();
 
-        // Server Address
         URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        // Driver Initialization
         driver = new AndroidDriver(serverURL, options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        // Open the page in Chrome
         driver.get("https://v1.training-support.net/selenium");
     }
 
-    // Test method
     @Test
-    public void webAppTest() {
+    public void todoListTest() {
         Dimension dims = driver.manage().window().getSize();
+        Point start = new Point((int)(dims.getWidth() * 0.5), (int)(dims.getHeight() * 0.8));
+        Point end = new Point((int)(dims.getWidth() * 0.5), (int)(dims.getHeight() * 0.6));
 
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text='Get Started!']")));
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text=\"Get Started!\"]"))).click();
 
-        Point start = new Point((int) (dims.getWidth() * 0.5), (int) (dims.getHeight() * 0.8));
-        Point end = new Point((int) (dims.getWidth() * 0.5), (int) (dims.getHeight() * 0.6));
-        doSwipe(driver, start, end, 80);
+        ActionsBase.doSwipe(driver, start, end, 50);
 
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text='To-Do List']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]")));
 
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.EditText[@resource-id='taskInput']")));
+        int sizeBeforeTask = driver.findElements(AppiumBy.xpath("//android.widget.TextView")).size();
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Add tasks to list");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Get number of tasks");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Clear the list");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
 
-        WebElement addTaskInput = driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='taskInput']"));
-        WebElement addTaskButton = driver.findElement(AppiumBy.xpath("//android.widget.Button[@text='Add Task']"));
+        //Number of task
+        List<WebElement> tasks = driver.findElements(AppiumBy.xpath("//android.widget.TextView"));
+        Assert.assertEquals(tasks.size(), sizeBeforeTask+3);
 
-        addTaskInput.sendKeys("Add tasks to list");
-        addTaskButton.click();
-        addTaskInput.sendKeys("Get the number of tasks");
-        addTaskButton.click();
-        addTaskInput.sendKeys("Clear the list");
-        addTaskButton.click();
+        //Strike
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Add tasks to list\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Get number of tasks\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Clear the list\"]")).click();
 
-        WebElement clearListButton = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=' Clear List']"));
-
-        clearListButton.click();
-
-
-        // Tear down method
-
+        //Clear
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[3]")).click();
     }
 }
-
